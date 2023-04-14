48ed89cb8e51c2357aa98815178daf7a26f6de5c
Kevin <klums24@gmail.com>
2023-04-14 10:41:30 -0700

    phase 1 fetch lab complete

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
-}
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      console.log(renderBooks(data));
+      
+    })
+    return fetch("https://anapioficeandfire.com/api/books");
+  }
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +25,15 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+/*
+fetch("https://anapioficeandfire.com/books/5")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(`Book title: ${data["name"]}`);
+  });
+*/
+
